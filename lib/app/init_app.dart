7c248af0075ab68ba3eaec969c978a90807446a7
Ahmad Sattout <ahmad.sattout.ee@gmail.com>
2024-09-19 15:19:05 +0300

    Set System UI Settings in `initApp`

--- lib/app/init_app.dart
+++ lib/app/init_app.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 Future<void> initApp() async {
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent,
+    statusBarColor: Colors.transparent,
+  ));
+
   await initHydratedStorage();
 }
 
